2b8148e739dd196f8821d9af6cfe1f0e4ab43974
Tomás Urdiales <83011678+tomasurdiales@users.noreply.github.com>
2021-12-03 00:51:55 +0100

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 # Homebrew installation:
-echo("Installing Homebrew:")
+echo "Installing Homebrew:"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # (will ask for sudo authorization)
-
-# Update zsh shell and install on Homebrew path: /opt/homebrew/bin
-echo("Installing and updating zsh:")
-brew install zsh
-# (this is required to run the programs under Executables)
+echo
 
 # Oh My Zsh installation:
-echo("Installing Oh My Zsh:")
+echo "Installing Oh My Zsh:"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # (will ask for sudo authorization)
-
+echo
 
 # Rename GitHub folder:
-mv -r ~/ConfigFiles ~/TomsConfig
+echo "Renaming config folder to ~/TomsConfig..."
+mv ~/ConfigFiles-master ~/TomsConfig
+echo
 
 # Symlink all dotfiles:
-echo("Symlinking dotfiles to home directory:")
+echo "Symlinking dotfiles to home directory..."
+mv ~/.zshrc ~/zshrc_old
 ln -s ~/TomsConfig/.zshrc ~
 ln -s ~/TomsConfig/.zprofile ~
 ln -s ~/TomsConfig/.vimrc ~
 ln -s ~/TomsConfig/.pylintrc ~
 ln -s ~/TomsConfig/.gitconfig ~ # (these are my personal settings!)
 ln -s ~/TomsConfig/.gitignore ~
+echo
 
 ln -s ~/TomsConfig/env.sh ~
 
 # Symlink Executable programs:
-echo("Symlinking personal scripts to /usr/local/bin:")
+echo "Symlinking personal scripts to /usr/local/bin..."
 ln -s ~/TomsConfig/Executables/runmatlab /usr/local/bin
 ln -s ~/TomsConfig/Executables/cwhich /usr/local/bin
 ln -s ~/TomsConfig/Executables/userlogout /usr/local/bin
@@ -45,14 +45,23 @@ chmod +x /usr/local/bin/userlogout
 chmod +x /usr/local/bin/opensocials
 chmod +x /usr/local/bin/jupyterpdf
 chmod +x /usr/local/bin/jupyterpython
+echo
 
 
-# Run Brewfile to install all Homebrew casks and formulae:
-# (will take some time to go through everything)
-echo("Initialising installation of all Homebrew packages")
-cd ~/TomsConfig
-brew bundle
-# (requires that Brewfile be decrypted)
+# Update zsh shell and install on Homebrew path: /opt/homebrew/bin
+echo "Installing and updating zsh..."
+/opt/homebrew/bin/brew install zsh
+# (this is required to run the programs under Executables)
+echo
 
-echo("Setup completed")
-echo(":|")
+# # Run Brewfile to install all Homebrew casks and formulae:
+# # (will take some time to go through everything)
+# echo "Initialising installation of all Homebrew packages"
+# cd ~/TomsConfig
+# brew bundle
+# # echo
+# # (requires that Brewfile be decrypted)
+
+echo "Setup completed."
+echo ":|"
+echo
