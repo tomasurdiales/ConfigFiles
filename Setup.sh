e4a7b5b335b151c74971290130abf769ea27a2f5
Tomás Urdiales <83011678+tomasurdiales@users.noreply.github.com>
2021-12-17 15:37:55 +0100

    Edited .zshrc

--- Setup.sh
+++ Setup.sh
@@ -9,7 +9,6 @@ echo
 # Oh My Zsh installation:
 echo "Installing Oh My Zsh:"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# (will ask for sudo authorization)
 echo
 
 
